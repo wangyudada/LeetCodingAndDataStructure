425a38381ebb3021540404883f24062d523cb6de
wangy <a667281@qq.com>
2021-08-11 10:39:05 +0800

    [描述]力扣第15题《三数之和》 [作者]王宇 [时间]2021/8/10晚

---
+++ src/Day6/Demo.java
@@ -0,0 +1,45 @@
+package Day6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) return res;
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = length- 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[left], nums[right], nums[i]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+}
