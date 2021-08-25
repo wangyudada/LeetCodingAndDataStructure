25ec914adaa3792518963d36272952827829d5df
WangYu <a667281@qq.com>
2021-08-25 22:13:39 +0800

    [描述]力扣第36题《有效的数独》，本题子数独的序号是重点 [作者]王宇 [时间]2021/8/25晚

---
+++ src/day15/Demo.java
@@ -0,0 +1,51 @@
+package day15;
+
+import java.util.HashMap;
+
+/**
+ * 请你判断一个9x9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+ * <p>
+ * 数字1-9在每一行只能出现一次。
+ * 数字1-9在每一列只能出现一次。
+ * 数字1-9在每一个以粗实线分隔的3x3宫内只能出现一次。（请参考示例图）
+ * 数独部分空格内已填入了数字，空白格用'.'表示。
+ * <p>
+ * 注意：
+ * <p>
+ * 一个有效的数独（部分已被填充）不一定是可解的。
+ * 只需要根据以上规则，验证已经填入的数字是否有效即可。
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValidSudoku(char[][] board) {
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer>[] columns = new HashMap[9];
+        HashMap<Integer, Integer>[] boxes = new HashMap[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashMap<>();
+            columns[i] = new HashMap<>();
+            boxes[i] = new HashMap<>();
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int n = (int) c;
+//                    子数独的序号
+                    int box_idx = (i / 3) * 3 + j / 3;
+                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+                    columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
+                    boxes[box_idx].put(n, boxes[box_idx].getOrDefault(n, 0) + 1);
+
+                    if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_idx].get(n) > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
