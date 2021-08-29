7011c25ec9d1b658638d18e55f146e703b88d3ed
WangYu <a667281@qq.com>
2021-08-29 22:27:16 +0800

    [描述]二分法的Java实现 [作者]王宇 [时间]2021/8/29晚

--- src/day6/Demo.java
+++ src/day6/Demo.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ */
 public class Demo {
     public static void main(String[] args) {
 
@@ -20,7 +30,7 @@ public class Demo {
             }
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1;
-            int right = length- 1;
+            int right = length - 1;
             while (left < right) {
                 int sum = nums[left] + nums[right] + nums[i];
                 if (sum == 0) {
