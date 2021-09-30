70b3fc38e3d7987cb985ca040ec57b44a71eebc6
wangy <a667281@qq.com>
2021-09-30 16:07:42 +0800

    [描述]力扣第62《不同路径》 [作者]王宇 [时间]2021/9/30下午

--- src/month2/day3/Test.java
+++ src/month2/day3/Test.java
@@ -2,9 +2,12 @@ package month2.day3;
 
 public class Test {
     public static void main(String[] args) {
-        Demo demo = new Demo();
-        int[] nums = {2,3,1,1,4};
-        boolean b = demo.canJump(nums);
-        System.out.println(b);
+//        Demo demo = new Demo();
+//        int[] nums = {2, 3, 1, 1, 4};
+//        boolean b = demo.canJump(nums);
+//        System.out.println(b);
+        Demo2 demo2 = new Demo2();
+        int i = demo2.uniquePaths(3, 2);
+        System.out.println(i);
     }
 }
