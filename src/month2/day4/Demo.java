2b6af032b0947d300919268a8f765fb499c2014f
WangYu <a667281@qq.com>
2021-10-05 16:47:17 +0800

    [描述]62. 不同路径 [作者]王宇 [时间]2021/9/29晚

---
+++ src/month2/day4/Demo.java
@@ -0,0 +1,39 @@
+package month2.day4;
+
+/**
+ * 62. 不同路径
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * <p>
+ * 问总共有多少条不同的路径？
+ * 示例 1：
+ * <p>
+ * 输入：m = 3, n = 2
+ * 输出：3
+ * 解释：
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向下
+ * https://leetcode-cn.com/problems/unique-paths/
+ *
+ * @author WangYu
+ */
+public class Demo {
+    public int uniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+}
