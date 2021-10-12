6d19ce1839977a7bd701fbbfa4619a54ea56da8c
wangy <a667281@qq.com>
2021-10-12 19:55:44 +0800

    [描述]95. 不同的二叉搜索树2 [作者]王宇 [时间]2021/10/8下午

---
+++ src/month2/day7/Demo.java
@@ -0,0 +1,22 @@
+package month2.day7;
+
+import java.util.List;
+
+/**
+ * 95. 不同的二叉搜索树 II
+ * 给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
+ * 示例 1：
+ * 输入：n = 3
+ * 输出：[[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：[[1]]
+ * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/
+ *
+ * @author WangYu
+ */
+public class Demo {
+    public List<TreeNode> generateTrees(int n) {
+
+    }
+}
---
+++ src/month2/day7/Test.java
@@ -0,0 +1,7 @@
+package month2.day7;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
