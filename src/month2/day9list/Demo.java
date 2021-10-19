a6d3f4f9252a198d021fa9abdf3a63fd465548d4
wangy <a667281@qq.com>
2021-10-19 20:19:47 +0800

    [描述]206. 反转链表+链表 [作者]王宇 [时间]2021/10/19下午

--- src/month2/day9list/Demo.java
+++ src/month2/day9list/Demo.java
@@ -16,23 +16,6 @@ package month2.day9list;
  * @author WangYu
  */
 public class Demo {
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(ListNode next, int val) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
--- src/month2/day9list/Test.java
+++ src/month2/day9list/Test.java
@@ -2,6 +2,14 @@ package month2.day9list;
 
 public class Test {
     public static void main(String[] args) {
-
+        Demo demo = new Demo();
+        ListNode listNode = new ListNode();
+        ListNode listNode1 = new ListNode(listNode,5);
+        ListNode listNode2 = new ListNode(listNode1, 4);
+        ListNode listNode3 = new ListNode(listNode2, 3);
+        ListNode listNode4 = new ListNode(listNode3, 2);
+        ListNode listNode5 = new ListNode(listNode4, 1);
+        ListNode listNode6 = demo.reverseList(listNode);
+        System.out.println(listNode6);
     }
 }
