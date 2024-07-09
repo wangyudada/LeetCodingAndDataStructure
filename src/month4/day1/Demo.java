77b373f9672d4490e0d9b306677a04db8a39cdbf
WangYu <a667281@qq.com>
2024-07-09 22:51:15 +0800

    刷刷刷刷刷刷刷刷

---
+++ src/month4/day1/Demo.java
@@ -0,0 +1,7 @@
+package month4.day1;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/month4/day1/Solution.java
@@ -0,0 +1,30 @@
+package month4.day1;
+
+import month2.day16.ListNode;
+
+public class Solution {
+    /**
+     * https://leetcode.cn/problems/linked-list-cycle-ii/submissions/545345744/
+     *
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
+}
