962c1f77ad0f42044a27f041b15bd2c277868e4e
WangYu <a667281@qq.com>
2024-07-30 22:46:23 +0800

    刷刷刷刷刷刷刷刷

---
+++ src/month4/day9/Demo.java
@@ -0,0 +1,7 @@
+package month4.day9;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/month4/day9/Solution.java
@@ -0,0 +1,47 @@
+package month4.day9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    /**
+     * https://leetcode.cn/problems/3sum/
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (right > left) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (right > left && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while (right > left && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+
+    }
+}
